74148a23ac499fd45c526c3f14b5a158e01b88db
uwjatin <unikweb.jatin@gmail.com>
2026-03-14 17:20:05 +0530

    initial comit
    initail commit

---
+++ server/chunks/build/styles.mjs
@@ -0,0 +1,12 @@
+const interopDefault = r => r.default || r || [];
+const styles = {
+  "pages/index.vue": () => import('./index-styles.CJhUE803.mjs').then(interopDefault),
+  "pages/index.vue?vue&type=style&index=0&scoped=0f8c1c0d&lang.css": () => import('./index-styles.CJhUE803.mjs').then(interopDefault),
+  "node_modules/nuxt/dist/app/components/error-404.vue": () => import('./error-404-styles.DtuF77Et.mjs').then(interopDefault),
+  "node_modules/nuxt/dist/app/components/error-500.vue": () => import('./error-500-styles.3g_bWYX3.mjs').then(interopDefault),
+  "node_modules/nuxt/dist/app/components/error-404.vue?vue&type=style&index=0&scoped=8b7948c5&lang.css": () => import('./error-404-styles.DtuF77Et.mjs').then(interopDefault),
+  "node_modules/nuxt/dist/app/components/error-500.vue?vue&type=style&index=0&scoped=d7edb62b&lang.css": () => import('./error-500-styles.3g_bWYX3.mjs').then(interopDefault)
+};
+
+export { styles as default };
+//# sourceMappingURL=styles.mjs.map
